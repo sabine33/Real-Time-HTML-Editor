b936c4a2cd431b9fa25df80c442386fe8fe87000
sabin khanal <sabin.khanal.33@gmail.com>
2022-06-15 18:38:21 +0545

    New Update Wed, Jun 15, 2022  6:38:21 PM

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-eval `ssh-agent` && ssh-add ~/github && git add . && git commit -m "New Update $currentDate" && git push origin master 
+eval `ssh-agent` && ssh-add ~/github && git add . && git commit -m "New Update `date`" && git push origin master 
